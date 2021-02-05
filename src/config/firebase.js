140ccfa3913081b99a23222af64088fb31a1f82b
Serge Byusa <sergebyusa@Serges-MacBook-Pro.local>
2021-02-05 13:09:46 -0800

    initialized a new repo

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBSD7sWEyQhLA3Z13GNcGuI0xHJLAnNvpM",
+  authDomain: "whatsapp-clone-a0260.firebaseapp.com",
+  projectId: "whatsapp-clone-a0260",
+  storageBucket: "whatsapp-clone-a0260.appspot.com",
+  messagingSenderId: "861358568517",
+  appId: "1:861358568517:web:931bd30aa220327ac35f2c",
+  measurementId: "G-KF66C3X8QZ",
+};
